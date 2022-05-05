ef5ec310005c612e0496b12a9a8acc733f867a1f
Adrien1306 <adrienfouga@live.fr>
2022-05-05 14:27:41 +0200

    mise en forme

--- js/script.js
+++ js/script.js
@@ -5,18 +5,25 @@ const allItemsContainer = document.querySelector("#allItems")
 
 //On affiche tous les articles de "livres" dans itemContainer
 livres.forEach(element => {
-    const itemContainer = document.createElement("article")
-    allItemsContainer.appendChild(itemContainer)
-    itemContainer.innerHTML += `
-    <h2>${element.name}</h2>
-    <img src="${element.image}" alt="">
-    <p> Catégorie : ${element.category}</p>
-    <p>${element.description}</p>
-    <p> ID : ${element.id}</p>
-    <p> Prix : ${element.price}</p>
-    <p> Quantité : ${element.quantity}</p>
+   
+    allItemsContainer.innerHTML += `
+    <article class="col">
+        <div class="card card-body">
+            <h2>${element.name}</h2>
+            <img src="${element.image}" alt="">
+            <p> Catégorie : ${element.category}</p>
+            <p>${element.description}</p>
+            <p> ID : ${element.id}</p>
+            <p> Prix : ${element.price}</p>
+            <p> Quantité : ${element.quantity}</p>
+            <a href="#" class="btn btn-primary">Mettre dans le panier</a>
+        </div>
+       
+    </article>
     `
 });
+
+
 
 
 function displayAllBooks() {
